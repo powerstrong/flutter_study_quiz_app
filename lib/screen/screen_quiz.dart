8fce9c8c8584da2f78fe62f42de4ea3a93532e30
jslee <jslee@plaif.com>
2022-12-05 00:20:18 +0900

    2강 : 퀴즈모델, 퀴즈풀기 화면 -경고가 많이 뜨는데 일단 넘어가자. -RaisedButton -> ElevatedButton

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app_test/widget/widget_cadidate.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import '../model/model_quiz.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -13,8 +16,9 @@ class QuizScreen extends StatefulWidget {
 
 class _QuizScreenState extends State<QuizScreen> {
   List<int> _answers = [-1, -1, -1];
-  List<bool> _answerState = [false, false, false];
+  List<bool> _answerState = [false, false, false, false];
   int _currentIndex = 0;
+  SwiperController _swiperController = SwiperController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,15 +30,136 @@ class _QuizScreenState extends State<QuizScreen> {
         backgroundColor: Colors.deepPurple,
         body: Center(
           child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              border: Border.all(color: Colors.deepPurple),
-            ),
-            width: width * 0.85,
-            height: height * 0.5,
-          ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(color: Colors.deepPurple),
+              ),
+              width: width * 0.85,
+              height: height * 0.5,
+              child: Swiper(
+                controller: _swiperController,
+                physics: NeverScrollableScrollPhysics(),
+                loop: false,
+                itemCount: widget.quizs.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildQuizCard(widget.quizs[index], width, height);
+                },
+              )),
         ),
       ),
     );
+  }
+
+  Widget _buildQuizCard(Quiz quiz, double width, double height) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        border: Border.all(
+          color: Colors.white,
+        ),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q${_currentIndex + 1}.',
+              style: TextStyle(
+                fontSize: width * 0.06,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                fontSize: width * 0.048,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            children: _buildCandidates(width, quiz),
+          ),
+          Container(
+            padding: EdgeInsets.all(width * 0.024),
+            child: Center(
+              child: ButtonTheme(
+                minWidth: width * 0.5,
+                height: height * 0.05,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: ElevatedButton(
+                  child: _currentIndex == widget.quizs.length - 1
+                      ? Text('결과보기')
+                      : Text('다음문제'),
+                  onPressed: _answers[_currentIndex] == -1
+                      ? null
+                      : () {
+                          if (_currentIndex == widget.quizs.length - 1) {
+                          } else {
+                            setState(() {
+                              _currentIndex++;
+                              _answers[_currentIndex] = -1;
+                              _answerState = [false, false, false, false];
+                              _swiperController.next();
+                            });
+                          }
+                        },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple,
+                    onPrimary: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+    for (int i = 0; i < 4; i++) {
+      _children.add(
+        CandWidget(
+          index: i,
+          title: quiz.candidates[i],
+          width: width,
+          answerState: _answerState[i],
+          tap: () {
+            setState(() {
+              for (int j = 0; j < 4; j++) {
+                if (j == i) {
+                  _answerState[j] = true;
+                  _answers[_currentIndex] = j;
+                } else {
+                  _answerState[j] = false;
+                }
+              }
+            });
+          },
+        ),
+      );
+      _children.add(
+        Padding(
+          padding: EdgeInsets.all(width * 0.024),
+        ),
+      );
+    }
+
+    return _children;
   }
 }
